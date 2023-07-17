ae4465a1abc42f9c7dcbcb21d99d9b9f563eaf5e
Madhav007 <madhavaprashath669@gmail.com>
2023-07-17 22:47:50 +0530

    Added functionalities

--- backend/routes/contactRoutes.js
+++ backend/routes/contactRoutes.js
@@ -20,23 +20,27 @@ router.post('/identify', async (req, res) => {
     const query1 = `SELECT * FROM contacts WHERE email = '${email}'`;
     const query2 = `SELECT * FROM contacts WHERE phoneNumber = '${phoneNumber}'`;
 
-    let findRecordByEmail,findRecordByphoneNumber;
+    var findRecordByEmail=[],findRecordByphoneNumber=[];
 
-    try{
-
-        findRecordByEmail = await query(query1);
-        console.log("findRecordByEmail are: ",findRecordByEmail);
-    }catch(err){
-        console.log(err);
-        return res.status(500).json({error:'Internal Server Error'});
+    if(email){
+        try{
+    
+            findRecordByEmail = await query(query1);
+            //console.log("findRecordByEmail are: ",findRecordByEmail);
+        }catch(err){
+            console.log(err);
+            return res.status(500).json({error:'Internal Server Error'});
+        }
     }
 
-    try{
-        findRecordByphoneNumber = await query(query2);
-        console.log("findRecordByphoneNumber are: ",findRecordByphoneNumber);
-    }catch(err){
-        console.log(err);
-        return res.status(500).json({error:'Internal Server Error'});
+    if(phoneNumber){
+        try{
+            findRecordByphoneNumber = await query(query2);
+            //console.log("findRecordByphoneNumber are: ",findRecordByphoneNumber);
+        }catch(err){
+            console.log(err);
+            return res.status(500).json({error:'Internal Server Error'});
+        }
     }
 
       
@@ -70,7 +74,150 @@ router.post('/identify', async (req, res) => {
             console.log(err);
             return res.status(500).json({error:'Internal Server Error'});
           }
+
+     }
+    else{   
+        
+        try{
+            let query3;
+            query3 = `SELECT * FROM contacts WHERE (email = '${email}' AND email IS NOT NULL) OR (phoneNumber = '${phoneNumber}' AND phoneNumber IS NOT NULL);`;
+
+            
+            const contacts = await query(query3);
+            console.log("Contacts are: ",contacts);
+            
+            const primaryContacts = contacts.filter(contact => contact.linkPrecedence === "primary");
+            const secondaryContacts = contacts.filter(contact => contact.linkPrecedence === "secondary");4
+            console.log("Primary Contacts are: ",primaryContacts);
+            console.log("Secondary Contacts are: ",secondaryContacts);
+            var exactMatchFound=false;
+
+            let primaryContactId,primaryPhoneNumber,primaryEmail;
+            let emails = [];
+            let phoneNumbers = [];
+            let secondaryContactIds = [];
+
+            
+
+            if(primaryContacts.length == 1){
+                primaryContactId = primaryContacts[0].id;
+                primaryPhoneNumber = primaryContacts[0].phoneNumber;
+                primaryEmail = primaryContacts[0].email;
+                if(primaryPhoneNumber) phoneNumbers.push(primaryPhoneNumber);
+                if(primaryEmail) emails.push(primaryEmail);
+
+                secondaryContacts.forEach(contact => {  
+                    if(contact.email === email  && contact.phoneNumber === phoneNumber) exactMatchFound = true;
+                    if(contact.email && !emails.includes(contact.email)) emails.push(contact.email);
+                    if(contact.phoneNumber && !phoneNumbers.includes(contact.phoneNumber)) phoneNumbers.push(contact.phoneNumber);
+                    if(!secondaryContactIds.includes(contact.id)) secondaryContactIds.push(contact.id);
+                });
+
+                if(!exactMatchFound){  // Add a new secondary contact.
+                    let newContact = {
+                        phoneNumber: phoneNumber,
+                        email: email,
+                        linkedId: primaryContactId,
+                        linkPrecedence: 'secondary',
+                        createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
+                        updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
+                      };
+                      const insertQuery = `INSERT INTO contacts (phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt)
+                                 VALUES ('${newContact.phoneNumber}', '${newContact.email}', ${newContact.linkedId}, '${newContact.linkPrecedence}', '${newContact.createdAt}', '${newContact.updatedAt}')`;
+
+                    const result = await query(insertQuery);
+                    console.log("New secondary data contact is inserted....: ",result);
+
+                    secondaryContactIds.push(result.insertId);
+                    if(email && !emails.includes(email)) emails.push(email);
+                    if(phoneNumber && !phoneNumbers.includes(phoneNumber)) phoneNumbers.push(phoneNumber);
+                }
+
+                const responseData = {
+                    "contact":{
+                        "primaryContactId":primaryContactId,
+                        "emails": emails,
+                        "phoneNumbers" : phoneNumbers,
+                        "secondaryContactIds" : secondaryContactIds
+                    }
+                }
+                return res.status(200).json(responseData);
+            }
+            else if(primaryContacts.length == 2){  // To do. The last case.
+                query3 = `UPDATE contacts SET linkPrecedence="secondary",linkedId=${primaryContacts[0].id},
+                            updatedAt='${new Date().toISOString().slice(0, 19).replace('T', ' ')}' WHERE id=${primaryContacts[1].id}`;
+                    console.log(query3);
+                    const result = await query(query3);
+                    primaryContactId = primaryContacts[0].id;
+                    primaryEmail = primaryContacts[0].email;
+                    primaryPhoneNumber = primaryContacts[0].phoneNumber;
+                    if(primaryPhoneNumber) phoneNumbers.push(primaryPhoneNumber);
+                    if(primaryEmail) emails.push(primaryEmail);
+                    if(!phoneNumbers.includes(primaryContacts[1].phoneNumber))  phoneNumbers.push(primaryContacts[1].phoneNumber);
+                if(!emails.includes(primaryContacts[1].email)) emails.push(primaryContacts[1].email);
+                secondaryContactIds.push(primaryContacts[1].id);
+                const responseData = {
+                    "contact":{
+                        "primaryContactId":primaryContactId,
+                        "emails": emails,
+                        "phoneNumbers" : phoneNumbers,
+                        "secondaryContactIds" : secondaryContactIds
+                    }
+                }
+                return res.status(200).json(responseData);
+
+            }else{
+                primaryContactId = secondaryContacts[0].linkedId;
+                query3 = `SELECT * FROM contacts WHERE id=${primaryContactId}`;
+                const primaryObject = await query(query3);
+                primaryEmail = primaryObject[0].email;
+                primaryPhoneNumber = primaryObject[0].phoneNumber;
+                if(primaryPhoneNumber) phoneNumbers.push(primaryPhoneNumber);
+                if(primaryEmail) emails.push(primaryEmail);
+                
+                secondaryContacts.forEach(contact => {  
+                    if(contact.email === email  && contact.phoneNumber === phoneNumber) exactMatchFound = true;
+                    if(contact.email && !emails.includes(contact.email)) emails.push(contact.email);
+                    if(contact.phoneNumber && !phoneNumbers.includes(contact.phoneNumber)) phoneNumbers.push(contact.phoneNumber);
+                    if(!secondaryContactIds.includes(contact.id)) secondaryContactIds.push(contact.id);
+                });
+
+                if(!exactMatchFound){  // Add a new secondary contact.
+                    let newContact = {
+                        phoneNumber: phoneNumber,
+                        email: email,
+                        linkedId: primaryContactId,
+                        linkPrecedence: 'secondary',
+                        createdAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
+                        updatedAt: new Date().toISOString().slice(0, 19).replace('T', ' '),
+                      };
+                      const insertQuery = `INSERT INTO contacts (phoneNumber, email, linkedId, linkPrecedence, createdAt, updatedAt)
+                                 VALUES ('${newContact.phoneNumber}', '${newContact.email}', ${newContact.linkedId}, '${newContact.linkPrecedence}', '${newContact.createdAt}', '${newContact.updatedAt}')`;
+
+                    const result = await query(insertQuery);
+                    console.log("New secondary data here contact is inserted....: ",result);
+
+                    secondaryContactIds.push(result.insertId);
+                    if(email && !emails.includes(email)) emails.push(email);
+                    if(phoneNumber && !phoneNumbers.includes(phoneNumber)) phoneNumbers.push(phoneNumber);
+                }
+
+                const responseData = {
+                    "contact":{
+                        "primaryContactId":primaryContactId,
+                        "emails": emails,
+                        "phoneNumbers" : phoneNumbers,
+                        "secondaryContactIds" : secondaryContactIds
+                    }
+                }
+                return res.status(200).json(responseData);
+            }    
+        }catch(err){
+            console.log(err);
+            return res.status(500).json({error:'Internal Server Error'});
+        }
     }
+
     return res.status(200).json({message:'Done'});
 });
 
